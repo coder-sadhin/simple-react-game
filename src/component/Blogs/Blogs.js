be89cbf06949cdb040272c919848696ec0c6979f
coder-sadhin <codersadhin@gmail.com>
2022-09-29 18:22:30 +0600

    blogs section responsive

--- src/component/Blogs/Blogs.js
+++ src/component/Blogs/Blogs.js
@@ -3,18 +3,21 @@ import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div className='blog-container'>
-            <div>
-                <h2>How does react work?</h2>
-                <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
-            </div>
-            <div>
-                <h2>Props vs state in react</h2>
-                <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.</p>
-            </div>
-            <div>
-                <h2>How many work with useEffect can be used?</h2>
-                <p>You can have multiple useEffects in your code and this is completely fine! As hooks docs say, you should separate concerns. Multiple hooks rule also applies to useState - you can have multiple useState in one component to separate different part of the state, you don't have to build one complicated state object.</p>
+        <div>
+            <h2 className='blog-title'>Inportent Qustion Answer</h2>
+            <div className='blog-container'>
+                <div className='blog'>
+                    <h2>How does react work?</h2>
+                    <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+                </div>
+                <div className='blog'>
+                    <h2>Props vs state in react</h2>
+                    <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.</p>
+                </div>
+                <div className='blog'>
+                    <h2>How many work with useEffect can be used?</h2>
+                    <p>You can have multiple useEffects in your code and this is completely fine! As hooks docs say, you should separate concerns. Multiple hooks rule also applies to useState - you can have multiple useState in one component to separate different part of the state, you don't have to build one complicated state object.</p>
+                </div>
             </div>
         </div>
     );
